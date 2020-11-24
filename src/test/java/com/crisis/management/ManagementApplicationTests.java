6291664e5f3fcabfeb740f3eebb50adf9df9c29b
Marcin Kroczak <marcink1303@gmail.com>
2020-11-24 09:53:39 +0100

    initial commit

---
+++ src/test/java/com/crisis/management/ManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisis.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
